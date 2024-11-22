e66b61f29c0a9c21357ac97088c3ac7ea93282a2
Josh Cirre <joshcirre@gmail.com>
2024-11-22 15:42:56 -0700

    change title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,25 +4,25 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Roundest Pokemon' }}</title>
+    <title>{{ $title ?? 'Roundest – Laravel Version (TALL Stack)' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body
     class="flex flex-col justify-between min-h-screen antialiased text-white border-t-2 border-purple-600 min-w-screen bg-gray-950">
     <header class="px-8 py-4">
-        <div class="flex justify-between items-baseline">
+        <div class="flex items-baseline justify-between">
             <div class="flex items-center">
                 <a href="/" class="text-3xl font-bold">
                     round<span class="text-red-500">est</span>
                 </a>
-                <span class="flex items-baseline pt-1 pl-2 text-sm font-extralight text-gray-400">
+                <span class="flex items-baseline pt-1 pl-2 text-sm text-gray-400 font-extralight">
                     <p>(<a href="https://tallstack.dev" target="_blank" rel="noopener noreferrer"
                             class="hover:underline">TALL Stack</a> Version)
                     </p>
                 </span>
             </div>
-            <nav class="flex flex-row gap-8 items-center">
+            <nav class="flex flex-row items-center gap-8">
                 <a href="/results" class="text-lg hover:underline">
                     Results
                 </a>
