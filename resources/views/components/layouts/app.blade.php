c71dc41304865c06f58249f6e79dc55aaed726b9
Josh Cirre <joshcirre@gmail.com>
2024-11-22 15:50:40 -0700

    update title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Roundest – Laravel Version (TALL Stack)' }}</title>
+    <title>{{ $title ?? 'Roundest (Laravel Version)' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
